df6c53d14555840fd64c0d357520bd58e3a5c888
nazi sharifi <banonazisharifi@gmail.com>
2025-10-10 20:43:49 +0430

    commit

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Hero2 from '../Componnents/Hero2'
 import Navbar from '../Componnents/Navbar'
+import AboutFlower from '../Componnents/AboutFlower'
+import OurStory from '../Componnents/OurStory'
 // import Hero from '../Componnents/Hero'
 
 function Home() {
   return (
-    <div>
-      {/* <Hero/> */}
+    <div className='bg-stone-50'>
       <Navbar/>
+      {/* <Hero/> */}
       <Hero2/>
+      <AboutFlower/>
+      <OurStory/>
     </div>
   )
 }
