d8d35356bfa3022684940257c13bffe707b30c8a
nazi sharifi <banonazisharifi@gmail.com>
2025-10-13 22:11:35 +0430

    some Componnint  and some style added

--- src/Componnents/OurTeam.tsx
+++ src/Componnents/OurTeam.tsx
@@ -35,15 +35,15 @@ function OurTeam() {
         },
     ]
   return (
-    <div className='w-full h-fit px-12 py-24 grid grid-cols-3 gap-24 items-center justify-center'>
+    <div className='w-full h-fit py-24 grid grid-cols-3 gap-16 items-center justify-center'>
         {Team.map((team,index)=>(
-        <div key={index} className='h-[80vh] w-[420px] relative group'>
+        <div key={index} className='h-[80vh] w-[400px] relative group'>
             <img src={team.Img} className='h-full w-full  transition-all duration-700 ease-linear' alt="" />
             <div className='w-[320px] h-28 flex flex-col z-30 items-center absolute -bottom-12 right-14 bg-white justify-center shadow-lg shadow-gray-400'>
                 <h1 className='text-3xl font-Headeing'>{team.Name}</h1>
                 <h3 className='text-xl text-[#D83054]'>{team.Job}</h3>
             </div>
-            <div className='h-[80vh] w-[420px] absolute top-0 right-0 transition-all duration-1000 ease-linear bg-black/40 opacity-0 group-hover:opacity-100 flex justify-center items-center space-x-4'>
+            <div className='h-[80vh] w-[400px] absolute top-0 right-0 transition-all duration-1000 ease-linear bg-black/40 opacity-0 group-hover:opacity-100 flex justify-center items-center space-x-4'>
             <button className='bg-[#D83054] text-white rounded-full p-4'>
             <Linkedin size={24}/>
             </button>
