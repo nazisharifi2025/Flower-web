fc8bf16b761f61c2861ee8de5271f395c800824c
nazi sharifi <banonazisharifi@gmail.com>
2025-10-14 11:59:57 +0430

    some Componnint  and some style added

--- src/Pages/Protfolio.tsx
+++ src/Pages/Protfolio.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from '../Componnents/Navbar'
+import HeroPro from '../Componnents/HeroPro'
+import Protfoliopost from '../Componnents/Protfoliopost'
+import Instagram from '../Componnents/Instagram'
+import Footer from '../Componnents/Footer'
+import Counter from '../Componnents/Counter'
 
 function Protfolio() {
   return (
-    <div>Protfolio</div>
+    <div>
+      <Navbar/>
+      <HeroPro/>
+      <Protfoliopost/>
+      <Counter/>
+      <Instagram/>
+      <Footer/>
+    </div>
   )
 }
 
